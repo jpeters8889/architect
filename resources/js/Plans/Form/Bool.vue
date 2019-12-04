38fb2a55c0975e7d80ced6ed18445954786eccfc
jamie peters <jamie.phpeters@gmail.com>
2019-12-04 19:47:59 +0000

    reworked boolean component

--- resources/js/Plans/Form/Bool.vue
+++ resources/js/Plans/Form/Bool.vue
@@ -1,8 +1,14 @@
 <template>
-    <select class="form-control form-control-input w-full" :name="name" v-model="actualValue">
-        <option value="0">No</option>
-        <option value="1">Yes</option>
-    </select>
+    <div>
+        <input type="hidden" :name="name" v-model="actualValue" />
+
+        <div v-bind:class="[actualValue==='1' ? 'border-highlight justify-end' : '', 'border-3 flex w-15 bg-5 rounded-full items-center p-1']"
+             @click="toggleValue()"
+        >
+            <div class="w-6 h-6 rounded-full bg-7" v-bind:class="[actualValue==='1' ? 'hidden' : '', 'w-6 h-6 rounded-full bg-7']"></div>
+            <div class="w-6 h-6 rounded-full bg-highlight" v-bind:class="[actualValue==='0' ? 'hidden' : '', 'w-6 h-6 rounded-full bg-7']"></div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -11,10 +17,16 @@
     export default {
         mixins: [IsAFormField],
 
-        // mounted() {
-        //   if(this.actualValue==='') {
-        //       this.actualValue = '0';
-        //   }
-        // },
+        mounted() {
+            if (this.actualValue === null) {
+                this.actualValue = '0';
+            }
+        },
+
+        methods: {
+            toggleValue() {
+                this.actualValue = this.actualValue === '1' ? '0' : '1';
+            }
+        }
     }
 </script>
